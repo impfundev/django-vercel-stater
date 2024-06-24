5643e39a0d5932e3096a8241c61c60f82c99155a
impfundev <mail@ilhammaulana.me>
2024-06-24 17:27:39 +0700

    feat: REST API

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,13 @@
 # example/urls.py
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from example.views import index, UserViewSet, GroupViewSet
 
-from example.views import index
-
+router = routers.DefaultRouter()
+router.register(r"users", UserViewSet)
+router.register(r"groups", GroupViewSet)
 
 urlpatterns = [
-    path('', index),
+    path("", index),
+    path("api/v1/", include(router.urls)),
 ]
--- example/views.py
+++ example/views.py
@@ -1,16 +1,39 @@
-# example/views.py
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
 from datetime import datetime
-
 from django.http import HttpResponse
+
+from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+
+    queryset = Group.objects.all().order_by("name")
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
 
 def index(request):
     now = datetime.now()
-    html = f'''
+    html = f"""
     <html>
         <body>
             <h1>Hello from Vercel!</h1>
             <p>The current time is { now }.</p>
         </body>
     </html>
-    '''
+    """
     return HttpResponse(html)
